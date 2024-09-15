7883874f0a97f88dd69108f2c017b1bf26af2f02
Sanjay Choudhary <iamsanjaych@gmail.com>
2024-09-15 16:37:38 +0530

    add pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import { Button } from './components/ui/button';
 import AppLayout from './layout/app-layout';
 import Landing from './pages/Landing';
 import Onboarding from './pages/Onboarding';
+import JobListing from './pages/JobListing';
+import JobPage from './pages/JobPage';
+import PostJob from './pages/PostJob';
+import SavedJobs from './pages/SavedJobs';
+import MyApplications from './pages/MyApplications';
 
 const router = createBrowserRouter([
    {
@@ -16,7 +21,27 @@ const router = createBrowserRouter([
       {
         path:'/onboarding',
         element:<Onboarding/>
-      }
+      },
+      {
+        path:'/jobs',
+        element:<JobListing/>
+      },
+      {
+        path:'/job/:id',
+        element:<JobPage/>
+      },
+      {
+        path:'/post-job',
+        element:<PostJob/>
+      },
+      {
+        path:'/saved-job',
+        element:<SavedJobs/>
+      },
+      {
+        path:'/my-jobs',
+        element:<MyApplications/>
+      },
      ]
    },
 ])
