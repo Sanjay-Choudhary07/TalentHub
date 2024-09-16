28f48c05a68127793a31eaa0f4e79ef8b05449ea
Sanjay Choudhary <iamsanjaych@gmail.com>
2024-09-16 11:02:47 +0530

    setup superbase

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo-lg.png';
 import {Button} from './ui/button'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
 const Header = () => {
   return (
     <nav className='py-4 flex justify-between items-center'>
       <Link>
       <img src={logo} className='h-20'/>
       </Link>
-      <Button varient="outline">Login</Button>
+      {/* <Button varient="outline">Login</Button> */}
+      <SignedOut>
+        <SignInButton/>
+      </SignedOut>
+      <SignedIn>
+        <UserButton/>
+      </SignedIn>
     </nav>
   )
 }
