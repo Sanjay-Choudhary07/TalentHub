4e00a037cc038db515f14f7351a18e3feb52502a
Sanjay Choudhary <iamsanjaych@gmail.com>
2024-09-16 09:49:59 +0530

    add header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo-lg.png';
+import {Button} from './ui/button'
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <nav className='py-4 flex justify-between items-center'>
+      <Link>
+      <img src={logo} className='h-20'/>
+      </Link>
+      <Button varient="outline">Login</Button>
+    </nav>
   )
 }
 
