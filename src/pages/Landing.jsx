f88ac9f93149f0681a521ea8f1b9fbacb2ea1966
Sanjay Choudhary <iamsanjaych@gmail.com>
2024-09-24 19:00:29 +0530

    update json

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,6 +2,31 @@ import React from 'react'
 import logo from '../assets/logo-lg.png'
 import { Link } from 'react-router-dom'
 import { Button } from '@/components/ui/button'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem
+} from "@/components/ui/carousel"
+
+const companies = [
+  { name: "amazon", path: amazonLogo, id: 1 },
+  { name: "atlassian", path: atlassianLogo, id: 2 },
+  { name: "google", path: googleLogo, id: 3 },
+  { name: "ibm", path: ibmLogo, id: 4 },
+  { name: "meta", path: metaLogo, id: 5 },
+  { name: "microsoft", path: microsoftLogo, id: 6 },
+  { name: "netflix", path: netflixLogo, id: 7 },
+  { name: "uber", path: uberLogo, id: 8 },
+];
+import amazonLogo from '../assets/amazon.svg';
+import atlassianLogo from '../assets/atlassian.svg';
+import googleLogo from '../assets/google.png';
+import ibmLogo from '../assets/ibm.svg';
+import metaLogo from '../assets/meta.svg';
+import microsoftLogo from '../assets/microsoft.svg';
+import netflixLogo from '../assets/netflix.png';
+import uberLogo from '../assets/uber.svg';
+import Autoplay from 'embla-carousel-autoplay'
 const Landing = () => {
   return (
     <main className='flex flex-col gap-10 sm:gap-20 py-10 sm:py-10'>
@@ -25,7 +50,19 @@ const Landing = () => {
         <Button variant="destructive" size="xl">Post a Job</Button>
         </Link>
       </div>
-      
+      <Carousel plugins={[Autoplay({delay:2000})]}
+      className="w-full py-10"
+    >
+      <CarouselContent className='flex gap-5 sm:gap-20 items-center'>
+        {companies.map(({name,id,path})=>{
+          return(
+            <CarouselItem key={id} className='basis-1/3 lg:basis-1/6'>
+              <img src={path} alt={name} className='h-9 sm:h-14 object-contain'/>
+            </CarouselItem>
+          );
+        })}
+      </CarouselContent>
+    </Carousel>
     </main>
   )
 }
