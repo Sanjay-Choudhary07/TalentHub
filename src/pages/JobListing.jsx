46d44f97845ca5e86439b8c31e9d6b5293a7384e
Sanjay Choudhary <iamsanjaych@gmail.com>
2024-09-28 18:11:35 +0530

    job page

--- src/pages/JobListing.jsx
+++ src/pages/JobListing.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
-
+import { useSession } from '@clerk/clerk-react'
+import React, { useEffect } from 'react'
+import { getJobs } from '@/api/apiJobs';
 const JobListing = () => {
+   const {session} = useSession();
+   const fetchJobs = async()=>{
+    const supabaseAccessToken = await session.getToken({
+      template:"supabase",
+    });
+    const data = await getJobs(supabaseAccessToken);
+    console.log(data);
+   };
+   useEffect(()=> {}, []);
   return (
     <div>
       JobListing
